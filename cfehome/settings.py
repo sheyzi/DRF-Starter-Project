547088f46711a049d600d200fb4320393407bf47
sheyzi <gistkiosk@gmail.com>
2023-12-01 05:24:52 +0100

    Added ability to set allowed hosts from the .env

--- cfehome/settings.py
+++ cfehome/settings.py
@@ -30,8 +30,8 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env("DEBUG")
 
-ALLOWED_HOSTS = []
 
+ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", default=["127.0.0.1", "localhost"])
 
 # Application definition
 
