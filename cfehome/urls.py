acf1a58596a54790710eac0ae79f8d1c6246613e
sheyzi <gistkiosk@gmail.com>
2023-12-01 05:50:01 +0100

    Added url static file configuration

--- cfehome/urls.py
+++ cfehome/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -13,3 +15,7 @@ urlpatterns = [
     ),
     path("api/inventory/", include("inventory.urls")),
 ]
+
+
+# Setup for serving static files
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
